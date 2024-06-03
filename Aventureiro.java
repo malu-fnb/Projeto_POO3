b2763dcfbc28f09368ae9f897926a7287987942a
malu-fnb <134156256+malu-fnb@users.noreply.github.com>
2024-06-03 16:35:08 -0300

    Update Aventureiro.java

--- Aventureiro.java
+++ Aventureiro.java
@@ -67,23 +67,104 @@ public class Aventureiro {
 		this.tesourosColetados = tesourosColetados;
 	}
 
-	public void moverNorte(Labirinto labirinto, int tamanho) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void moverNorte(Labirinto labirinto, int tamanho) {
+        if (energia <= 0) {
+            System.out.println("Energia insuficiente para mover.");
+            return;
+        }
 
-	public void moverSul(Labirinto labirinto, int tamanho) {
-		// TODO Auto-generated method stub
-		
-	}
+        int novaLinha = linha - tamanho;
+        int novaColuna = coluna;
 
-	public void moverLeste(Labirinto labirinto, int tamanho) {
-		// TODO Auto-generated method stub
-		
-	}
+        if (labirinto.isDentroDoLabirinto(novaLinha, novaColuna)) {
+            this.linha = novaLinha;
+            this.coluna = novaColuna;
+            this.energia -= 1;
+            System.out.println(nome + " se moveu para o Norte. Nova posição: (" + linha + ", " + coluna + "). Energia restante: " + energia);
+            labirinto.atualizarPosicaoAventureiro(novaLinha, novaColuna);
+        } else {
+            System.out.println("Movimento inválido! Fora do labirinto.");
+        }
+    }
 
-	public void moverOeste(Labirinto labirinto, int tamanho) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void moverSul(Labirinto labirinto, int tamanho) {
+        if (energia <= 0) {
+            System.out.println("Energia insuficiente para mover.");
+            return;
+        }
+
+        int novaLinha = linha + tamanho;
+        int novaColuna = coluna;
+
+        if (labirinto.isDentroDoLabirinto(novaLinha, novaColuna)) {
+            this.linha = novaLinha;
+            this.coluna = novaColuna;
+            this.energia -= 1;
+            System.out.println(nome + " se moveu para o Sul. Nova posição: (" + linha + ", " + coluna + "). Energia restante: " + energia);
+            labirinto.atualizarPosicaoAventureiro(novaLinha, novaColuna);
+        } else {
+            System.out.println("Movimento inválido! Fora do labirinto.");
+        }
+    }
+
+    public void moverLeste(Labirinto labirinto, int tamanho) {
+        if (energia <= 0) {
+            System.out.println("Energia insuficiente para mover.");
+            return;
+        }
+
+        int novaLinha = linha;
+        int novaColuna = coluna + tamanho;
+
+        if (labirinto.isDentroDoLabirinto(novaLinha, novaColuna)) {
+            this.linha = novaLinha;
+            this.coluna = novaColuna;
+            this.energia -= 1;
+            System.out.println(nome + " se moveu para o Leste. Nova posição: (" + linha + ", " + coluna + "). Energia restante: " + energia);
+            labirinto.atualizarPosicaoAventureiro(novaLinha, novaColuna);
+        } else {
+            System.out.println("Movimento inválido! Fora do labirinto.");
+        }
+    }
+
+    public void moverOeste(Labirinto labirinto, int tamanho) {
+        if (energia <= 0) {
+            System.out.println("Energia insuficiente para mover.");
+            return;
+        }
+
+        int novaLinha = linha;
+        int novaColuna = coluna - tamanho;
+
+        if (labirinto.isDentroDoLabirinto(novaLinha, novaColuna)) {
+            this.linha = novaLinha;
+            this.coluna = novaColuna;
+            this.energia -= 1;
+            System.out.println(nome + " se moveu para o Oeste. Nova posição: (" + linha + ", " + coluna + "). Energia restante: " + energia);
+            labirinto.atualizarPosicaoAventureiro(novaLinha, novaColuna);
+        } else {
+            System.out.println("Movimento inválido! Fora do labirinto.");
+        }
+    }
+
+    public void descansar() {
+        if (energia >= 10) {
+            System.out.println("Você já está descansado, deixe de preguiça e siga seu caminho.");
+        } else {
+            this.energia += 2;
+            System.out.println(nome + " descansou e recuperou 2 de energia. Energia atual: " + energia);
+        }
+    }
+
+    public void interagir(Labirinto labirinto) {
+        System.out.println("Interagindo...");
+    }
+
+    public ArrayList<Tesouro> getTesourosColetados() {
+        return tesourosColetados;
+    }
+
+    public void setTesourosColetados(ArrayList<Tesouro> tesourosColetados) {
+        this.tesourosColetados = tesourosColetados;
+    }
 }
