407f5b2a9bdba01f2f188dc4cb228f3e1698d33e
malu-fnb <134156256+malu-fnb@users.noreply.github.com>
2024-06-03 16:35:23 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,170 +1,61 @@
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.Scanner;
 
-public class Aventureiro {
-    private String nome;
-    private int posicao;
-    private int energia;
-    private ArrayList<Tesouro> tesourosColetados;
+public class Main {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
 
-    public Aventureiro(String nome) {
-        this.nome = nome;
-        this.posicao = 0; 
-        this.energia = 10; 
-        this.setTesourosColetados(new ArrayList<>());
-    }
+        System.out.println("Bem vindo, bravo aventureiro!, se é que esse é mesmo seu nome...");
+        System.out.println("Digite seu nome: ");
+        String nomeAventureiro = scanner.next();
+        Aventureiro aventureiro = new Aventureiro(nomeAventureiro);
 
-    public String getNome() {
-        return nome;
-    }
+        while (true) {
+            System.out.println("Escolha a dificuldade do labirinto (1-Fácil, 2-Médio, 3-Difícil): ");
+            int escolha = scanner.nextInt();
+            int tamanho = 20;
 
-    public int getEnergia() {
-        return energia;
-    }
-
-    public void mover(Labirinto labirinto, int base) {
-        if (energia <= 0) {
-            System.out.println("Energia insuficiente para mover.");
-            return;
-        }
-
-        Random rand = new Random();
-        int novaPosicao = posicao + rand.nextInt(6) + 1; 
-
-        try {
-            if (labirinto.isDentroDoLabirinto(novaPosicao, 0)) {
-                this.posicao = novaPosicao;
-                System.out.println(nome + " se moveu para a posição " + novaPosicao + ". Energia restante: " + energia);
-                labirinto.atualizarPosicaoAventureiro(novaPosicao, base / 2); 
-            } else {
-                throw new Exception("Movimento inválido! Fora do labirinto.");
+            switch (escolha) {
+                case 1:
+                    tamanho = 20;
+                    break;
+                case 2:
+                    tamanho = 30;
+                    break;
+                case 3:
+                    tamanho = 40;
+                    break;
+                default:
+                    System.out.println("Opção inválida! Usando dificuldade fácil.");
+                    break;
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+
+            Labirinto labirinto = new Labirinto(tamanho);
+
+            while (aventureiro.getEnergia() > 0) {
+                labirinto.printarLabirinto();
+                System.out.println("Escolha sua ação aventureiro: \n1. Mover-se\n2. Descansar\n3. Interagir\n4. Sair");
+                int ops = scanner.nextInt();
+            
+                switch (ops) {
+                    case 1:
+                        aventureiro.mover(labirinto, tamanho);
+                        break;
+                    case 2:
+                        aventureiro.descansar();
+                        break;
+                    case 3:
+                        aventureiro.interagir(labirinto);
+                        break;
+                    case 4:
+                        System.out.println("Saindo do jogo...");
+                        return;
+                    default:
+                        System.out.println("Opção inválida! Tente novamente.");
+                }
+            }
+
+            System.out.println("Você morreu!\n\n\n Reiniciando o jogo...");
         }
-    }
-
-
-    public void descansar() {
-        if(energia>=10){
-            System.out.println("você ja esta descansado, deixe de preguiça e siga seu caminho");
-        }
-        else{
-            this.energia += 2;
-            System.out.println(nome + " descansou e recuperou 2 de energia. Energia atual: " + energia);
-        }
-    }
-
-    public void interagir(Labirinto labirinto) {
-        System.out.println("Interagindo...");
-    }
-
-	public ArrayList<Tesouro> getTesourosColetados() {
-		return tesourosColetados;
-	}
-
-	public void setTesourosColetados(ArrayList<Tesouro> tesourosColetados) {
-		this.tesourosColetados = tesourosColetados;
-	}
-
-    public void moverNorte(Labirinto labirinto, int tamanho) {
-        if (energia <= 0) {
-            System.out.println("Energia insuficiente para mover.");
-            return;
-        }
-
-        int novaLinha = linha - tamanho;
-        int novaColuna = coluna;
-
-        if (labirinto.isDentroDoLabirinto(novaLinha, novaColuna)) {
-            this.linha = novaLinha;
-            this.coluna = novaColuna;
-            this.energia -= 1;
-            System.out.println(nome + " se moveu para o Norte. Nova posição: (" + linha + ", " + coluna + "). Energia restante: " + energia);
-            labirinto.atualizarPosicaoAventureiro(novaLinha, novaColuna);
-        } else {
-            System.out.println("Movimento inválido! Fora do labirinto.");
-        }
-    }
-
-    public void moverSul(Labirinto labirinto, int tamanho) {
-        if (energia <= 0) {
-            System.out.println("Energia insuficiente para mover.");
-            return;
-        }
-
-        int novaLinha = linha + tamanho;
-        int novaColuna = coluna;
-
-        if (labirinto.isDentroDoLabirinto(novaLinha, novaColuna)) {
-            this.linha = novaLinha;
-            this.coluna = novaColuna;
-            this.energia -= 1;
-            System.out.println(nome + " se moveu para o Sul. Nova posição: (" + linha + ", " + coluna + "). Energia restante: " + energia);
-            labirinto.atualizarPosicaoAventureiro(novaLinha, novaColuna);
-        } else {
-            System.out.println("Movimento inválido! Fora do labirinto.");
-        }
-    }
-
-    public void moverLeste(Labirinto labirinto, int tamanho) {
-        if (energia <= 0) {
-            System.out.println("Energia insuficiente para mover.");
-            return;
-        }
-
-        int novaLinha = linha;
-        int novaColuna = coluna + tamanho;
-
-        if (labirinto.isDentroDoLabirinto(novaLinha, novaColuna)) {
-            this.linha = novaLinha;
-            this.coluna = novaColuna;
-            this.energia -= 1;
-            System.out.println(nome + " se moveu para o Leste. Nova posição: (" + linha + ", " + coluna + "). Energia restante: " + energia);
-            labirinto.atualizarPosicaoAventureiro(novaLinha, novaColuna);
-        } else {
-            System.out.println("Movimento inválido! Fora do labirinto.");
-        }
-    }
-
-    public void moverOeste(Labirinto labirinto, int tamanho) {
-        if (energia <= 0) {
-            System.out.println("Energia insuficiente para mover.");
-            return;
-        }
-
-        int novaLinha = linha;
-        int novaColuna = coluna - tamanho;
-
-        if (labirinto.isDentroDoLabirinto(novaLinha, novaColuna)) {
-            this.linha = novaLinha;
-            this.coluna = novaColuna;
-            this.energia -= 1;
-            System.out.println(nome + " se moveu para o Oeste. Nova posição: (" + linha + ", " + coluna + "). Energia restante: " + energia);
-            labirinto.atualizarPosicaoAventureiro(novaLinha, novaColuna);
-        } else {
-            System.out.println("Movimento inválido! Fora do labirinto.");
-        }
-    }
-
-    public void descansar() {
-        if (energia >= 10) {
-            System.out.println("Você já está descansado, deixe de preguiça e siga seu caminho.");
-        } else {
-            this.energia += 2;
-            System.out.println(nome + " descansou e recuperou 2 de energia. Energia atual: " + energia);
-        }
-    }
-
-    public void interagir(Labirinto labirinto) {
-        System.out.println("Interagindo...");
-    }
-
-    public ArrayList<Tesouro> getTesourosColetados() {
-        return tesourosColetados;
-    }
-
-    public void setTesourosColetados(ArrayList<Tesouro> tesourosColetados) {
-        this.tesourosColetados = tesourosColetados;
     }
 }
